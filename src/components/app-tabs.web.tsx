1fe6291d1fafd142b70fc4f801426567215708e9
zenyinnaya-star <zenyinnaya@gmail.com>
2026-07-07 12:51:10 -0600

    initial commit

--- src/components/app-tabs.web.tsx
+++ src/components/app-tabs.web.tsx
@@ -49,7 +49,7 @@ export function TabButton({ children, isFocused, ...props }: TabTriggerSlotProps
 
 export function CustomTabList(props: TabListProps) {
   const scheme = useColorScheme();
-  const colors = Colors[scheme === 'unspecified' ? 'light' : scheme];
+  const colors = Colors[scheme ?? 'light'];
 
   return (
     <View {...props} style={styles.tabListContainer}>
@@ -65,7 +65,8 @@ export function CustomTabList(props: TabListProps) {
             <ThemedText type="link">Docs</ThemedText>
             <SymbolView
               tintColor={colors.text}
-              name={{ ios: 'arrow.up.right.square', web: 'link' }}
+              name="arrow.up.right.square"
+              fallback={<ThemedText style={{ color: colors.text }}>{'↗'}</ThemedText>}
               size={12}
             />
           </Pressable>
